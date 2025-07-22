d9f2aa30f5be71a3d1c52a7daea0664327d22281
gpeendje <gpeendje@gmail.com>
2025-07-22 15:32:25 +0000

    added test.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,5 +1,6 @@
 from .action import Action
 from .plugin import Plugin
 from .logger import Logger
+from .test import Test
 
-__all__ = ['Action', 'Plugin','Logger']
+__all__ = ['Action', 'Plugin','Logger', 'Test']
